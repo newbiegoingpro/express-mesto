d3dc589b466c75d1df6fdba2181d33876b981701
Nikolay Shvaika <hiptoz@yandex.ru>
2021-07-01 23:07:27 +0300

    corsV.1

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 app.use(cors());
+app.options('*', cors());
 app.use(requestLogger);
 app.get('/crash-test', () => {
   setTimeout(() => {
